b0b42f7d8391f2dd672626201babb688a8a7a2d2
Gurik Sachar <120414055+GurikSachar@users.noreply.github.com>
2024-03-18 00:00:12 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,19 +4,10 @@ const documentRoutes = require('./routes/documentRoutes')
 const userRoutes = require('./routes/userRoutes')
 const cors = require('cors')
 const express = require('express')
-const rateLimit = require('express-rate-limit')
 
 const app = express()
 
-const limiter = rateLimit({
-    limit: 50,
-    windowMs: 60*60*1000,
-    message: "Too many request from this IP, please try again in an hour",
-})
-
 app.use(cors())
-
-app.use(limiter)
 
 app.use(express.json())
 
