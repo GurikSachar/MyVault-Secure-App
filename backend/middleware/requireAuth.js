df448cc4b95c3a241c5157022022fd7126f85a5c
Gurik Sachar <120414055+GurikSachar@users.noreply.github.com>
2024-03-17 14:54:06 +0530

    Update requireAuth.js

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -6,7 +6,7 @@ const requireAuth = async(req, res, next) => {
 
 
     if(!authorization){
-        return res.status(401).json({error: 'Authoriation token required'})
+        return res.status(401).json({error: 'Authorization token required'})
     }
 
     const token = authorization.split(' ')[1]
