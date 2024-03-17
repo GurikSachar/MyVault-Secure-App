42633b8d453d3957b8d48f64bed314fd11370d1d
Gurik Sachar <120414055+GurikSachar@users.noreply.github.com>
2024-03-17 23:31:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,15 @@
-const { rateLimit } = require('express-rate-limit')
 require('dotenv').config()
 const mongoose = require('mongoose')
 const documentRoutes = require('./routes/documentRoutes')
 const userRoutes = require('./routes/userRoutes')
 const cors = require('cors')
-
 const express = require('express')
 
 const app = express()
 
-const limiter = rateLimit({
-    limit: 50,
-    windowMs: 60*60*1000,
-    message: "Too many request from this IP, please try again in an hour",
-})
-
 app.use(cors())
 
 app.use(express.json())
-
-app.use(limiter)
 
 app.use('/api/documents', documentRoutes)
 app.use('/api/users', userRoutes)
